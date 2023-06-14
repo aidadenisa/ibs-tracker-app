061283523c4870d2eb9b1473cf2f63b0983cefaf
aida <aida.denisa@gmail.com>
2023-06-14 11:26:40 +0530

    Removed old code from App.js

--- src/App.js
+++ src/App.js
@@ -29,69 +29,6 @@ const router = createBrowserRouter([
 ])
 
 const App = () => {
-
-  const [user, setUser ] = useState(null);
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect( () => {
-    if(!user) return;
-    const fetchUser = async () => {
-      const userData = await userService.getCurrentUserInfo();
-      setUser(userData);
-    }
-
-    fetchUser()
-      .catch(console.error);
-  }, [loggedIn]);
-
-  const handleLogin = () => {
-    setLoggedIn(true);
-  }
-
-  const fetchEvents = () => {
-    if(!user) return;
-    console.log('effect');
-    const fetchData = async () => {
-      const events = await eventsService.getEvents();
-      setEvents(events);
-      console.log(events);
-    }
-  
-    fetchData()
-      .catch(console.error);
-  }
-
-  const fetchRecords = () => {
-    if(!user) return;
-    console.log('records');
-    recordsService
-      .getRecords('63c056c5c3b3e5612cfc62fb')
-      .then(records => {
-        setRecords(records);
-        console.log(records);
-      })
-      .catch(console.error);
-  }
-  
-
-  const [events, setEvents] = useState([]);
-  const [records, setRecords] = useState([]);
-  const newRecord = {
-    userId: '63c056c5c3b3e5612cfc62fb',
-    eventId: '63b74c2cfe015ec88961f9a3',
-  };
-
-  // more info on effects https://devtrium.com/posts/async-functions-useeffect
-  useEffect(fetchEvents, []);
-  useEffect(fetchRecords, [])
-
-  const createRecord = async () => {
-    const newRecordData = await recordsService.createRecord(newRecord);
-    setRecords([...records, newRecordData]);
-  }
-
-  const login = false;
-
   return (
     <div className="App">
       <RouterProvider router={router}/>
