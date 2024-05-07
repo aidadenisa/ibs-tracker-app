c193de2a225a66a842af78b31b6e901b2a423397
aida <aida.denisa@gmail.com>
2024-05-07 18:49:44 +0300

    Some formatting changes after adding prettier in vs code

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
-import globals from "globals";
-import reactPlugin from "eslint-plugin-react";
-import cypressPlugin from "eslint-plugin-cypress";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import jsPlugin from "@eslint/js";
-import tsParser from "@typescript-eslint/parser";
+import globals from 'globals'
+import reactPlugin from 'eslint-plugin-react'
+import cypressPlugin from 'eslint-plugin-cypress'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import jsPlugin from '@eslint/js'
+import tsParser from '@typescript-eslint/parser'
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
-    ignores: ["dist/**", "node_modules/**", "bin/**", "build/**"],
+    ignores: ['dist/**', 'node_modules/**', 'bin/**', 'build/**'],
   },
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
@@ -24,14 +24,14 @@ export default [
         ...globals.node,
         ...globals.cypress,
         ...globals.react,
-        describe: "readonly",
-        test: "readonly",
-        expect: "readonly",
-        it: "readonly",
+        describe: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        it: 'readonly',
       },
     },
     plugins: {
-      "@typescript-eslint": tseslint,
+      '@typescript-eslint': tseslint,
       react: reactPlugin,
       cypress: cypressPlugin,
     },
@@ -39,34 +39,34 @@ export default [
       ...jsPlugin.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
       ...cypressPlugin.configs.recommended.rules,
-      indent: ["error", 2],
-      "linebreak-style": ["error", "unix"],
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
       quotes: [
-        "warn",
-        "single",
+        'warn',
+        'single',
         {
           allowTemplateLiterals: true,
         },
       ],
-      "jsx-quotes": ["error", "prefer-double"],
+      'jsx-quotes': ['error', 'prefer-double'],
       // if we use == instead of ===
-      eqeqeq: "error",
-      "arrow-spacing": ["error", { before: true, after: true }],
-      "object-curly-spacing": ["error", "always"],
-      "arrow-spacing": ["error", { before: true, after: true }],
+      eqeqeq: 'error',
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
       // deactivate a recommended rule
-      "no-console": 0,
-      // "no-unused-vars": "error",
+      'no-console': 0,
+      'no-unused-vars': 0,
 
       // suppress errors for missing 'import React' in files
-      "react/react-in-jsx-scope": "off",
+      'react/react-in-jsx-scope': 'off',
       // allow jsx syntax in js files (for next.js project)
-      "react/prop-types": 0,
+      'react/prop-types': 0,
 
-      "@typescript-eslint/explicit-function-return-type": 0,
-      "@typescript-eslint/explicit-module-boundary-types": 0,
-      "@typescript-eslint/no-unused-vars": 0,
-      "@typescript-eslint/no-extra-semi": 0,
+      '@typescript-eslint/explicit-function-return-type': 0,
+      '@typescript-eslint/explicit-module-boundary-types': 0,
+      '@typescript-eslint/no-unused-vars': 0,
+      '@typescript-eslint/no-extra-semi': 0,
     },
   },
-];
+]
--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,43 +1,36 @@
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import Button from './Button.tsx';
-import { describe, test, expect, vi } from 'vitest';
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Button from './Button.tsx'
+import { describe, test, expect, vi } from 'vitest'
 
 describe('<Button/>', () => {
-
   test('renders content', () => {
-    render(<Button
-      variant="primary"
-      label="Test Button"
-    />);
+    render(<Button variant="primary" label="Test Button" />)
 
-    const element = screen.getByText('Test Button');
-    expect(element).toBeDefined();
-  });
+    const element = screen.getByText('Test Button')
+    expect(element).toBeDefined()
+  })
 
   test('renders content (with CSS selector)', () => {
-    const { container } = render(<Button
-      variant="primary"
-      label="Test Button"
-    />);
+    const { container } = render(
+      <Button variant="primary" label="Test Button" />
+    )
 
-    const button = container.querySelector('.ibs-btn');
-    expect(button).toHaveTextContent('Test Button');
-  });
+    const button = container.querySelector('.ibs-btn')
+    expect(button).toHaveTextContent('Test Button')
+  })
 
   test('clicking the button calls the event handler once', async () => {
-    const mockHandler = vi.fn();
-    render(<Button
-      variant="primary"
-      label="Test Button"
-      onClick={mockHandler}
-    />);
+    const mockHandler = vi.fn()
+    render(
+      <Button variant="primary" label="Test Button" onClick={mockHandler} />
+    )
 
-    const user = userEvent.setup();
-    const button = screen.getByText('Test Button');
+    const user = userEvent.setup()
+    const button = screen.getByText('Test Button')
     await user.click(button)
 
     expect(mockHandler).toHaveBeenCalledOnce()
-  });
+  })
 })
